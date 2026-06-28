4d6df2def6d19a8e82711e02fdd3b357d1db8044
Yozakura Misaka <MisakaOi@outlook.com>
2026-06-28 22:17:12 +0800

    fix: 添加Logger的using引用

--- FontManager/Services/GitHubService.cs
+++ FontManager/Services/GitHubService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using FontManager.Models;
+using FontManager.Utils;
 using Newtonsoft.Json;
 
 namespace FontManager.Services
